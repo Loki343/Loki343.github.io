d8da013c8480a95c1a42ea33546d69f25cc43627
Kausik Bhaumik <kausikbhaumik42@gmail.com>
2023-01-11 00:06:57 +0530

    new

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import './navbar.css'
+import React from "react";
+import "./navbar.css";
 
 const Navbar = () => {
   return (
-    <section id='navbar'>Navbar Component</section>
-  )
-}
+    <header className="header">
+      <div className="logoDiv">
+        <h1 className="logo">
+          <a href="#home">Ai.</a>
+        </h1>
+      </div>
 
-export default Navbar
+      <div className="navBar">
+        <ul className="navLists">
+          <li className="navItem">
+            <a href="#home" className="navLink">
+              1.<span>Home</span>
+            </a>
+          </li>
+          <li className="navItem">
+            <a href="#about" className="navLink">
+              2.<span>About</span>
+            </a>
+          </li>
+          <li className="navItem">
+            <a href="#skills" className="navLink">
+              3.<span>Skills</span>
+            </a>
+          </li>
+          <li className="navItem">
+            <a href="#projects" className="navLink">
+              4.<span>Projects</span>
+            </a>
+          </li>
+          <li className="navItem">
+            <a href="#about" className="navLink">
+              1.<span>About</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;

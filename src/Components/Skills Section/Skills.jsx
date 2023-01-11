237598e3f8a2c2853a9d80360a20c5399197185d
Kausik Bhaumik <kausikbhaumik42@gmail.com>
2023-01-11 13:57:01 +0530

    skills

--- src/Components/Skills Section/Skills.jsx
+++ src/Components/Skills Section/Skills.jsx
@@ -3,7 +3,25 @@ import './skills.css'
 
 const Skills = () => {
   return (
-    <section id='skills'>Skills Component</section>
+    <section id='skills' className='skills container section'>
+
+<div className="sectionTitle">
+        <span className="titleNumber">02.</span>
+        <h5 className="titleText">
+          Skills{" "}
+          <div className="underline">
+            {" "}
+            <span></span>{" "}
+          </div>
+        </h5>
+      </div>
+
+      {/* skills container */}
+      <div className="skillsContainer grid">
+        
+      </div>
+
+    </section>
   )
 }
 
